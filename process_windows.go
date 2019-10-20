5f64b6d1724d35928e788a3f0904e86e28f5501a
izumin5210 <m@izum.in>
2019-10-20 19:44:31 +0900

    Fix Process.Terminate() impl for windows

--- process_windows.go
+++ process_windows.go
@@ -5,6 +5,8 @@ import (
 	"os/exec"
 	"strconv"
 	"syscall"
+
+	"golang.org/x/sys/windows"
 )
 
 func newOSProcess(cmd *exec.Cmd) Process {
@@ -20,8 +22,45 @@ func newOSProcess(cmd *exec.Cmd) Process {
 }
 
 func (p *process) Terminate() error {
-	// https://github.com/Songmu/timeout/blob/v0.4.0/timeout_windows.go#L18-L20
-	return p.cmd.Process.Signal(p.Signal())
+	// https://github.com/mattn/goreman/blob/v0.3.4/proc_windows.go#L16-L55
+	dll, err := windows.LoadDLL("kernel32.dll")
+	if err != nil {
+		return err
+	}
+	defer dll.Release()
+
+	pid := p.cmd.Process.Pid
+
+	f, err := dll.FindProc("AttachConsole")
+	if err != nil {
+		return err
+	}
+	r1, _, err := f.Call(uintptr(pid))
+	if r1 == 0 && err != syscall.ERROR_ACCESS_DENIED {
+		return err
+	}
+
+	f, err = dll.FindProc("SetConsoleCtrlHandler")
+	if err != nil {
+		return err
+	}
+	r1, _, err = f.Call(0, 1)
+	if r1 == 0 {
+		return err
+	}
+	f, err = dll.FindProc("GenerateConsoleCtrlEvent")
+	if err != nil {
+		return err
+	}
+	r1, _, err = f.Call(windows.CTRL_BREAK_EVENT, uintptr(pid))
+	if r1 == 0 {
+		return err
+	}
+	r1, _, err = f.Call(windows.CTRL_C_EVENT, uintptr(pid))
+	if r1 == 0 {
+		return err
+	}
+	return nil
 }
 
 func (p *process) Kill() error {
